2f213042fdc2bcbcf3ffb07a6320cbae9395311d
wjlxyz <pkunewking@126.com>
2018-12-05 09:47:30 +0800

    add some dependencies of python

--- preparations.sh
+++ preparations.sh
@@ -1,4 +1,10 @@
 # !bin/sh
 
-# install bazel and swig
+# brew install bazel and swig
 brew install bazel swig
+
+# easy_install some dependency tools of python
+sudo easy_install -U six
+sudo easy_install -U numpy
+sudo easy_install wheel
+sudo easy_install ipython
